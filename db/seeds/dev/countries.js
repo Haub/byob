8714096b9a0d5355f5335fea2bb179a45f8ef256
Casey Hoepner <caseyhoepner@gmail.com>
2018-12-03 17:09:20 -0700

    Setup database and seed it

---
+++ db/seeds/dev/countries.js
@@ -0,0 +1,27 @@
+exports.seed = function(knex, Promise) {
+  return knex('demographics').del() 
+    .then(() => knex('countries').del()) 
+
+    .then(() => {
+      return Promise.all([
+        
+        knex('countries').insert({
+          dest_country: 'USA', grand_total: 20000
+        }, 'id')
+        .then(country => {
+          return knex('demographics').insert([
+            { 
+              origin_country: 'Somalia', 
+              individual_total: 20000, 
+              total_minors: 8000, 
+              dest_country_id: country[0] 
+            }
+            // { origin_country: 'Sudan', individual_total: 10000, total_minors: 4000, dest_country_id: country[0] }
+          ])
+        })
+        .then(() => console.log('Seeding complete!'))
+        .catch(error => console.log(`Error seeding data: ${error}`))
+      ]) 
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`));
+};
