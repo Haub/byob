68985d7246d9b5e5684819e10e50806aa31701d1
Casey Hoepner <caseyhoepner@gmail.com>
2018-12-05 14:20:18 -0700

    update knexfile

--- knexfile.js
+++ knexfile.js
@@ -10,17 +10,12 @@ module.exports = {
     },
     useNullAsDefault: true
   },
-
   production: {
-  client: 'pg',
-  connection: process.env.DATABASE_URL + `?ssl=true`,
-  migrations: {
-    directory: './db/migrations'
-  },
-  useNullAsDefault: true
-  }
-
-  test: {
-
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
   }
 };
